60ee81197536357e8afec49e703e2e6b170221b0
crimsonKangaroo <crimsonKangaroo@users.noreply.github.com>
2014-05-02 09:17:25 -0400

    Added Stun
    Game1 end of the stun.
    
    By the way, this is calling for the "BobbyBoxshoes" sprite, but I haven't figured out how to add the sprite to Git - if you can figure it out, you should have a copy of the sprite in your content folder that I copied on your USB drive. Otherwise I'm a bit at a loss (but I'll give it a few more tries)

--- MACGUFFIN_PROJECT/TestGame/TestGame/Game1.cs
+++ MACGUFFIN_PROJECT/TestGame/TestGame/Game1.cs
@@ -46,13 +46,14 @@ namespace TestGame
         Character char4;
 
         Rectangle startPlace;
-
+        
         KeyboardState kState;
 
         Texture2D guffin;
         Rectangle guffinPlace;
 
         // Players & respective interfaces
+        List<Player> players = new List<Player>();
         Player player1;
         UserInterface user1;
         Player player2;
@@ -75,6 +76,8 @@ namespace TestGame
         public bool guffinOn1 = false;
         bool guffinOn2 = false;
 
+        Boolean gameOver;
+        
         public Game1()
             : base()
         {
@@ -124,13 +127,26 @@ namespace TestGame
 
         // THIS IS BY FAR NOT THE BEST WAY TO DO THIS
         // IT DOESNT EVEN CHANGE THE PLACE
+        // will be put in Update Method
+        // Will add Macguffins to the board
+        // THIS IS BY FAR NOT THE BEST WAY TO DO THIS
+        // IT DOESNT EVEN CANGE THE PLACE
+
         public void AddGuffin()
         {
-            if (guffinOn1 == false)
-            {
-                guffinPlace = new Rectangle(rgen.Next(1, 700), rgen.Next(200, 300), 11, 18);
-                guffinOn1 = true;
-            }
+	        if (guffinOn1 == false)
+ 	        {
+		        foreach (Rectangle rect in walls)
+ 		        {
+                    guffinPlace = new Rectangle(rgen.Next(1, 700), rgen.Next(200, 300), 11, 18);
+			        while (rect.Intersects(guffinPlace))
+			        {
+ 				        guffinPlace.Y = rgen.Next(200, 350);
+ 				        guffinPlace.X = rgen.Next(1, 700);
+ 			        }
+ 			        guffinOn1 = true;
+		        }
+ 	        }
         }
 
         /// <summary>
@@ -145,33 +161,39 @@ namespace TestGame
             screenManager.AddScreen(new MainMenuScreen("Menu"), PlayerIndex.One);
             screenManager.Initialize();
             otherScreens = screenManager.GetScreens();
+            gameOver = false;
 
-            startPlace = new Rectangle(400, 300, 28, 62);
+            startPlace = new Rectangle(400, 300, 45, 62);
 
             player1 = new Player(1, Color.Red);
             user1 = new UserInterface(player1);
             char1 = new Character(player1);
-            char1.Place = new Rectangle(400, 300, 28, 62);
+            char1.Place = new Rectangle(300, 300, 45, 61);
 
             player2 = new Player(2, Color.Blue);
             user2 = new UserInterface(player2);
             char2 = new Character(player2);
-            char2.Place = new Rectangle(475, 300, 28, 62);
+            char2.Place = new Rectangle(375, 300, 45, 61);
 
             player3 = new Player(3, Color.Green);
             user3 = new UserInterface(player3);
             char3 = new Character(player3);
-            char3.Place = new Rectangle(550, 300, 28, 62);
+            char3.Place = new Rectangle(450, 300, 45, 61);
 
             player4 = new Player(4, Color.Yellow);
             user4 = new UserInterface(player4);
             char4 = new Character(player4);
-            char4.Place = new Rectangle(625, 300, 28, 62);
+            char4.Place = new Rectangle(525, 300, 45, 61);
 
             characters.Add(char1);
             characters.Add(char2);
             characters.Add(char3);
             characters.Add(char4);
+
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(player3);
+            players.Add(player4);
 
             // read the map
             ReadMap("map1.txt");
@@ -191,10 +213,10 @@ namespace TestGame
             block = Content.Load<Texture2D>("block1");
             text = Content.Load<SpriteFont>("Arial");
 
-            player1.Sprite = Content.Load<Texture2D>("player1");
-            player2.Sprite = Content.Load<Texture2D>("player1");
-            player3.Sprite = Content.Load<Texture2D>("player1");
-            player4.Sprite = Content.Load<Texture2D>("player1");
+            player1.Sprite = Content.Load<Texture2D>("BobbyBoxshoes");
+            player2.Sprite = Content.Load<Texture2D>("BobbyBoxshoes");
+            player3.Sprite = Content.Load<Texture2D>("BobbyBoxshoes");
+            player4.Sprite = Content.Load<Texture2D>("BobbyBoxshoes");
 
             guffin = Content.Load<Texture2D>("crystal");
         }
@@ -224,11 +246,27 @@ namespace TestGame
             kState = Keyboard.GetState();
 
             // calls the move method that has left and right movement and jumping
-            char1.Move(walls);
-            char2.Move(walls);
-            char3.Move(walls);
-            char4.Move(walls);
+            char1.Move(walls, gameTime);
+            char2.Move(walls, gameTime);
+            char3.Move(walls, gameTime);
+            char4.Move(walls, gameTime);
             AddGuffin();
+
+            // attacking logic
+
+            foreach (Character attacker in characters)
+            {
+                attacker.Attack();
+
+                foreach (Character victim in characters)
+                {
+                    if (attacker.StunBox.Intersects(victim.Place))
+                    {
+                        victim.Stunned(gameTime);
+                    }
+                }
+            }
+           
 
             // update the baton timer, if necessary (doesn't do anything for now)
             if (user1.StunCharge < 100)
@@ -252,10 +290,16 @@ namespace TestGame
             {
                 if (chr.Place.Y > GraphicsDevice.Viewport.Height + 500)
                 {
-                    chr.Place = startPlace;
+                    chr.Dead = true;
                 }
+
+                if (chr.Dead == true) chr.Respawn();
             }
 
+            foreach (Player pl in players)
+            {
+                if (pl.MacGuffinCount >= 10) gameOver = true;
+            }
             UpdateEnd:
             base.Update(gameTime);
         }
@@ -289,10 +333,10 @@ namespace TestGame
             /// Draw the players
             /// Right now, they all use player1's place (just for test purpose)
             /// </summary>
-            spriteBatch.Draw(player1.Sprite, char1.Place, Color.White);
-            spriteBatch.Draw(player2.Sprite, char2.Place, player2.PlayerColor);
-            spriteBatch.Draw(player3.Sprite, char3.Place, player3.PlayerColor);
-            spriteBatch.Draw(player4.Sprite, char4.Place, player4.PlayerColor);
+            char1.Draw(spriteBatch, player1.Sprite);
+            char2.Draw(spriteBatch, player1.Sprite);
+            char3.Draw(spriteBatch, player1.Sprite);
+            char4.Draw(spriteBatch, player1.Sprite);
 
             /// <summary>
             /// Display for P1 - P4
@@ -302,7 +346,25 @@ namespace TestGame
             user2.Draw(spriteBatch, guffin, baton, text);
             user3.Draw(spriteBatch, guffin, baton, text);
             user4.Draw(spriteBatch, guffin, baton, text);
-            
+
+            foreach (Character victim in characters)
+            {
+                    spriteBatch.Draw(guffin, victim.StunBox, Color.White);
+            }
+
+            if (char1.StunOn == true) spriteBatch.Draw(guffin, char1.StunBox, Color.Silver);
+
+            if (gameOver == true)
+            {
+                GraphicsDevice.Clear(Color.SkyBlue);
+                spriteBatch.DrawString(text, "GAME OVER", new Vector2(200,200), Color.SlateGray);
+                char1.CanMove = false;
+                char2.CanMove = false;
+                char3.CanMove = false;
+                char4.CanMove = false;
+                goto DrawEnd;
+            }
+
             DrawEnd:
             spriteBatch.End();
             base.Draw(gameTime);
